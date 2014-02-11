6e95b9571501a517f602543f89dbe4cbac2686be
Alexa <alexacgreenberg@gmail.com>
2014-02-11 04:32:41 -0500

    still designing

--- js/home.js
+++ js/home.js
@@ -1,19 +1,33 @@
 $(function () {
-	$('a').on('click',function(evt) {
+	$('#top-nav a').on('click',function(evt) {
 		evt.preventDefault();
 		var target = this.hash;
-		$('body').animate({
+		$('html').animate({
 			'scrollTop': $(target).offset().top
 		}, 1000, 'swing', function() {
 			window.location.hash = target;
 		});
 	});
 
-	// $(document).on('scroll', function() {
-	// 	if ($(this).scrollTop() > 80) {
-	// 		$("#top-nav").stop().animate({
-	// 			margin-top:50px;
-	// 		}, 600);
-	// 	}
-	// });
+	$(document).on('scroll', function() {
+		var currPos = $(this).scrollTop() + 50;
+		var navBar = $("top-nav");
+		var navBarHeight = navBar.outerHeight()+10;
+		var anchors = navBar.find("a");
+		var navItems = anchors.map(function() {
+			var anchor = $(this).attr("href");
+			if (item.length) { return item; }
+		});
+		if ($(this).scrollTop() > navBarHeight) {
+			$("#top-nav").stop().animate({
+				
+			}, 600);
+		}
+		
+		if (currPos > 200) {
+			$("#top-nav").addClass("dark-bg");
+		} else {
+			$("#top-nav").removeClass("dark-bg");
+		}
+	});
 });
